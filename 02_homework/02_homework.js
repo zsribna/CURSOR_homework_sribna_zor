e922c2efdf592ecc5afaa600f6c8a988a3c7b2bb
zsribna <zsribna@gmail.com>
2022-05-20 17:01:20 +0300

    kod2

---
+++ 02_homework/02_homework.js
@@ -0,0 +1,28 @@
+
+let n=prompt("введіть число ")
+n=Math.round(n);
+if(isnan(n)){
+    do{
+        alert('введіть ціле число!!!');
+     n= Math.round(prompt('введіть ціле число N знову:'));
+     if (isNaN(n)==false) {break}
+ } while (true);
+}
+let m=prompt("введіть число ")
+m=Math.round(m);
+if(isnan(m)){
+    do{
+        alert('введіть ціле число!!!');
+     m = Math.round(prompt('введіть ціле число M знову:'));
+     if (isNaN(m)==false) {break}
+ } while (true);
+}
+let sum=0;
+if (confirm('Чи пропустимо парні числа?')) {
+    for (let i= n; i <= m; i++) if(i % 2 !=0)
+    sum+=i;
+} else {
+    for (let i= n; i <= m; i++)
+    sum+=i
+  }
+alert(sum);
