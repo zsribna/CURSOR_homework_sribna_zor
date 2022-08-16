242336126aeebccd9ce18882b5472e6b11341481
zsribna <zsribna@gmail.com>
2022-08-16 12:13:46 +0300

    cod1

---
+++ 09_homework/09_homework.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const colorChenge = () => {
+  return Math.ceil(Math.random() * 256);
+};
+const color = () => {
+  return `rgb(${colorChenge()}, ${colorChenge()}, ${colorChenge()})`;
+};
+
+const generateBlocks = () => {
+  const main = document.createElement("div");
+  main.className = "main";
+  document.body.append(main);
+  for (let i = 1; i <= 25; i++) {
+    let block = document.createElement("div");
+    block.className = "blockStyle";
+    block.style.background = color();
+    main.append(block);
+  }
+};
+
+const generateBlocksInterval = () => {
+  const main = document.createElement("div");
+  main.className = "main";
+  document.body.append(main);
+  for (let i = 1; i <= 25; i++) {
+    let block = document.createElement("div");
+    block.className = "blockStyle";
+    setInterval(() => {
+      block.style.background = color();
+    }, 1000);
+
+    main.append(block);
+  }
+};
+
+generateBlocks();
+generateBlocksInterval();
